dca9f5c949400d72e5745cce16e38878400ae439
timmermansjoy <61321383+timmermansjoy@users.noreply.github.com>
2022-02-15 15:02:06 +0100

    almost working menu

---
+++ Mollen/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public int MaxSize = 5;
+    public int maxSpeed = 5;
+    public int maxHoles = 16;
+    public int maxPause = 5;
+    private int minNumber = 1;
+
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    public void Start()
+    {
+        int holes = PlayerPrefs.GetInt("holes");
+        GameObject.Find("HoleNumberIndicator").GetComponent<TMPro.TextMeshProUGUI>().text = "" + (holes);
+
+        int speed = PlayerPrefs.GetInt("speed");
+        GameObject.Find("SpeedNumberIndicator").GetComponent<TMPro.TextMeshProUGUI>().text = "" + (speed);
+
+        int pause = PlayerPrefs.GetInt("pauze");
+        GameObject.Find("PauseNumberIndicator").GetComponent<TMPro.TextMeshProUGUI>().text = "" + (pause);
+
+        int size = PlayerPrefs.GetInt("grootte");
+        GameObject.Find("SizeNumberIndicator").GetComponent<TMPro.TextMeshProUGUI>().text = "" + (size);
+    }
+
+
+    public void increaseSpeed()
+    {
+        int speed = PlayerPrefs.GetInt("speed");
+        if(speed <= maxSpeed)
+        {
+            PlayerPrefs.SetInt("speed",speed+1);
+            GameObject.Find("SpeedNumberIndicator").GetComponent<TMPro.TextMeshProUGUI>().text = "" + (speed + 1);
+        }
+    }
+
+    public void decreaseSpeed()
+    {
+        int speed = PlayerPrefs.GetInt("speed");
+        if(speed > minNumber)
+        {
+            PlayerPrefs.SetInt("speed",speed-1);
+            GameObject.Find("SpeedNumberIndicator").GetComponent<TMPro.TextMeshProUGUI>().text = "" + (speed - 1);
+        }
+    }
+
+
+    public void increasePauze()
+    {
+        int pauze = PlayerPrefs.GetInt("pauze");
+        if (pauze <= maxPause)
+        {
+            PlayerPrefs.SetInt("pauze", pauze + 1);
+            GameObject.Find("PauseNumberIndicator").GetComponent<TMPro.TextMeshProUGUI>().text = "" + (pauze + 1);
+        }
+    }
+
+    public void decreasePauze()
+    {
+        int pauze = PlayerPrefs.GetInt("pauze");
+        if(pauze > minNumber)
+        {
+            PlayerPrefs.SetInt("pauze", pauze - 1);
+            GameObject.Find("PauseNumberIndicator").GetComponent<TMPro.TextMeshProUGUI>().text = "" + (pauze - 1);
+        }
+
+    }
+
+    public void increasegrootte()
+    {
+        int grootte = PlayerPrefs.GetInt("grootte");
+        if(grootte <= MaxSize){
+            PlayerPrefs.SetInt("grootte", grootte + 1);
+            GameObject.Find("SizeNumberIndicator").GetComponent<TMPro.TextMeshProUGUI>().text = "" + (grootte + 1);
+        }
+    }
+
+    public void decreasegrootte()
+    {
+        int grootte = PlayerPrefs.GetInt("grootte");
+        if(grootte > minNumber)
+        {
+            PlayerPrefs.SetInt("grootte", grootte - 1);
+            GameObject.Find("SizeNumberIndicator").GetComponent<TMPro.TextMeshProUGUI>().text = "" + (grootte - 1);
+        }
+    }
+
+    public void increaseHoles()
+    {
+        int holes = PlayerPrefs.GetInt("holes");
+        if(holes <= maxHoles)
+        {
+            PlayerPrefs.SetInt("holes", holes + 1);
+            GameObject.Find("HoleNumberIndicator").GetComponent<TMPro.TextMeshProUGUI>().text = "" + (holes + 1);
+        }
+    }
+
+    public void decreaseHoles()
+    {
+        int holes = PlayerPrefs.GetInt("holes");
+        if(holes > minNumber)
+        {
+            PlayerPrefs.SetInt("holes", holes - 1);
+            GameObject.Find("HoleNumberIndicator").GetComponent<TMPro.TextMeshProUGUI>().text = "" + (holes - 1);
+        }
+    }
+
+
+
+}
