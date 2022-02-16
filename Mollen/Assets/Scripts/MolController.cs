82fc7a0667cedc1e73d84a726507210142386a3e
WiktorKosinskiPXL <11903939@student.pxl.be>
2022-02-16 09:28:10 +0100

    Working demo

---
+++ Mollen/Assets/Scripts/MolController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MolController : MonoBehaviour
+{
+    private Spawn spawner;
+    public float lifeTime = 5f;
+    public Sprite[] molSprites;
+    public GameObject molDoodPrefab;
+    void Start()
+    {
+        spawner = GameObject.Find("Spawner").GetComponent<Spawn>();
+        lifeTime = Random.Range(2f, 8f);
+        int soort = Random.Range(0, 10);
+        Sprite spr;
+        if (soort == 1)
+        {
+            spr = molSprites[1];
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.15f, 0);
+        }
+        else if (soort == 2)
+        {
+            spr = molSprites[2];
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.15f, 0);
+        }
+        else if (soort == 3)
+        {
+            spr = molSprites[3];
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.15f, 0);
+        }
+        else if (soort == 4)
+        {
+            spr = molSprites[4];
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.15f, 0);
+        }
+        else
+        {
+            spr = molSprites[0];
+        }
+        gameObject.GetComponent<SpriteRenderer>().sprite = spr;
+    }
+
+    void Update()
+    {
+        if (spawner.start)
+        {
+            lifeTime -= Time.deltaTime;
+        }
+        if (lifeTime < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnDestroy()
+    {
+        GameObject ded = Instantiate(molDoodPrefab) as GameObject;
+        ded.transform.position = transform.position;
+    }
+}
